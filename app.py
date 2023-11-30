8033502fa13c6fe3459c337eb5ec4517898a9e0d
lamassedegit <152498101+lamassedegit@users.noreply.github.com>
2023-11-30 13:33:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+app = Flask(__name__)
 
-app = Flask(__name)
+@app.route('/page2')
+def page2():
+    return '<h1>Hello, World!</h1>'
 
 @app.route('/')
-
 def hello_world():
-    return "Hello World"
+    return 'Hello, World!'
